7602db01a2609f7e2c0a5dd564d0de0fb4811c1a
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-05-05 13:20:57 +0200

    pass tracking uri to mlflow client

--- src/neptune_mlflow/export.py
+++ src/neptune_mlflow/export.py
@@ -35,7 +35,7 @@ MLFLOW_RUN_NAME_PROPERTY = "mlflow/run/name"
 
 
 def export_to_neptune(*, project: Project, mlflow_tracking_uri: str, include_artifacts: bool, max_artifact_size: int):
-    mlflow_client = mlflow.tracking.MlflowClient()
+    mlflow_client = mlflow.tracking.MlflowClient(tracking_uri=mlflow_tracking_uri)
     experiments = mlflow_client.search_experiments()
 
     experiment_ids = []
