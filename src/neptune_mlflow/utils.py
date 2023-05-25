0d1bc5d8da223691ae824b482e977207faf05c19
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-05-25 11:25:50 +0200

    implement dir upload strategy

--- src/neptune_mlflow/utils.py
+++ src/neptune_mlflow/utils.py
@@ -1,4 +1,11 @@
+__all__ = [
+    "ArtifactUploadStrategy",
+    "FileUploadStrategy",
+    "DirectoryUploadStrategy",
+]
+
 import os
+import shutil
 from abc import (
     ABC,
     abstractmethod,
@@ -23,6 +30,9 @@ class ArtifactUploadStrategy(ABC):
         self._uri = tracking_uri
         self._max_size = max_file_size
 
+    def _download_artifacts(self, run_id: str, path: str) -> str:
+        return download_artifacts(run_id=run_id, artifact_path=path, dst_path=".", tracking_uri=self._uri)
+
     @abstractmethod
     def upload_artifact(self, neptune_run: Run, info: FileInfo, run_id: str) -> None:
         ...
@@ -35,9 +45,7 @@ class FileUploadStrategy(ArtifactUploadStrategy):
 
         local_path = ""
         try:
-            local_path = download_artifacts(
-                run_id=run_id, artifact_path=info.path, dst_path=".", tracking_uri=self._uri
-            )
+            local_path = self._download_artifacts(run_id=run_id, path=info.path)
             local_path = Path(local_path).name
             neptune_run[f"{self.BASE_NAMESPACE}/{local_path}"].upload(local_path)
             neptune_run.wait()
@@ -48,5 +56,29 @@ class FileUploadStrategy(ArtifactUploadStrategy):
 
 
 class DirectoryUploadStrategy(ArtifactUploadStrategy):
+    def get_dir_size(self, path: str):
+        total = 0
+        with os.scandir(path) as it:
+            for entry in it:
+                if entry.is_file():
+                    total += entry.stat().st_size
+                elif entry.is_dir():
+                    total += self.get_dir_size(entry.path)
+        return total
+
     def upload_artifact(self, neptune_run: Run, info: FileInfo, run_id: str) -> None:
-        ...
+
+        # download directory first and then check the size
+        local_path = ""
+        try:
+            local_path = self._download_artifacts(run_id=run_id, path=info.path)
+
+            dir_size = self.get_dir_size(local_path)
+            if dir_size > self._max_size:
+                return
+            local_path = Path(local_path)
+            neptune_run[f"{self.BASE_NAMESPACE}/{local_path.name}"].upload_files(str(local_path / "*"))
+            neptune_run.wait()
+
+        finally:
+            shutil.rmtree(local_path)
