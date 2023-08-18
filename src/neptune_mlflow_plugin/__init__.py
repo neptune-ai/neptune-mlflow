472ca5c240bb5b45837b5ebfa9f78c192dab6825
AleksanderWWW <alwojnarowicz@gmail.com>
2023-08-18 18:27:56 +0200

    fix imports

--- src/neptune_mlflow_plugin/__init__.py
+++ src/neptune_mlflow_plugin/__init__.py
@@ -1,10 +1,7 @@
 # __all__ = ["MlflowPlugin"]
 #
 # from neptune_mlflow_plugin.impl import MlflowPlugin
-__all__ = ["NeptuneMlflowTracker", "create_neptune_tracking_uri"]
+__all__ = ["create_neptune_tracking_uri"]
 
 
-from neptune_mlflow_plugin.impl import (
-    NeptuneMlflowTracker,
-    create_neptune_tracking_uri,
-)
+from neptune_mlflow_plugin.impl import create_neptune_tracking_uri
