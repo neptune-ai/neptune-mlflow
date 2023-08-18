37cfd2a093f7b5f88888e93bd2e187458b42793a
AleksanderWWW <alwojnarowicz@gmail.com>
2023-08-18 17:39:01 +0200

    test two mlflow runs

--- tests/e2e/neptune_mlflow_plugin/test_neptune_plugin.py
+++ tests/e2e/neptune_mlflow_plugin/test_neptune_plugin.py
@@ -12,7 +12,7 @@ def test_e2e():
     mlflow.set_tracking_uri(uri)
 
     with mlflow.start_run() as run:
-        mlflow_run_id = run.info.run_id
+        mlflow_run_id_1 = run.info.run_id
 
         mlflow.set_tag("mytag", "myvalue")
         for i in range(10):
@@ -32,17 +32,22 @@ def test_e2e():
         ax.plot([0, 1], [2, 3])
 
         mlflow.log_figure(fig, "figure.png")
+
+    with mlflow.start_run() as run:
+        mlflow_run_id_2 = run.info.run_id
         mlflow.log_artifacts("docs", "my_docs")
 
-        mlflow.artifacts.download_artifacts(run_id=mlflow_run_id, artifact_path="my_docs", dst_path="downloaded_docs")
+        mlflow.artifacts.download_artifacts(run_id=mlflow_run_id_2, artifact_path="my_docs", dst_path="downloaded_docs")
 
         mlflow.log_artifact("CHANGELOG.md", "changelog")
 
         mlflow.artifacts.download_artifacts(
-            run_id=mlflow_run_id, artifact_path="changelog", dst_path="downloaded_changelog"
+            run_id=mlflow_run_id_2, artifact_path="changelog", dst_path="downloaded_changelog"
         )
 
-    with Run(custom_run_id=mlflow_run_id) as neptune_run:
+    assert mlflow_run_id_1 != mlflow_run_id_2  # make sure those are two different Neptune runs
+
+    with Run(custom_run_id=mlflow_run_id_1) as neptune_run:
         assert neptune_run["sys/tags"].fetch() == {"myvalue", "mytag"}
         assert float(neptune_run["lr"].fetch()) == 0.0001
         assert neptune_run.exists("figure")
@@ -50,5 +55,7 @@ def test_e2e():
         assert neptune_run.exists("image")
         assert neptune_run["some_key"].fetch_values().shape == (10, 3)
         assert neptune_run.exists("README")
+
+    with Run(custom_run_id=mlflow_run_id_2):
         assert os.path.isdir("downloaded_docs")
         assert os.path.isdir("downloaded_changelog")
