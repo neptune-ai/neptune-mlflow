15fa26dfec83b0d4ba5530771c8a8e6d60e09f69
AleksanderWWW <alwojnarowicz@gmail.com>
2023-08-18 18:36:15 +0200

    fix units

--- tests/unit/neptune_mlflow_plugin/test_impl.py
+++ tests/unit/neptune_mlflow_plugin/test_impl.py
@@ -3,7 +3,15 @@ from neptune_mlflow_plugin import create_neptune_tracking_uri
 
 def test_create_plugin_uri():
     uri = create_neptune_tracking_uri()
-    assert uri == "neptune-plugin://api_token=None/project=None/mode=async/name=Untitled/capture_stdout=True"
+    assert uri == (
+        "neptune-plugin://api_token=None/project=None/mode=async/name=Untitled/description=/capture_stdout"
+        "=True/capture_stderr=True/capture_hardware_metrics=True/fail_on_exception=True"
+        "/monitoring_namespace=monitoring/flush_period=5/capture_traceback=True"
+    )
 
     uri = create_neptune_tracking_uri(name="test_name")
-    assert uri == "neptune-plugin://api_token=None/project=None/mode=async/name=test_name/capture_stdout=True"
+    assert uri == (
+        "neptune-plugin://api_token=None/project=None/mode=async/name=test_name/description=/capture_stdout"
+        "=True/capture_stderr=True/capture_hardware_metrics=True/fail_on_exception=True"
+        "/monitoring_namespace=monitoring/flush_period=5/capture_traceback=True"
+    )
